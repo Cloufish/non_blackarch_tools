f6c96097aded4106fd17482d6e432b314251526f
Cloufish <bartmok17@gmail.com>
2021-01-30 09:21:51 +0100

    Added .api_keys.sh, added some wordlists and tools

--- .api_keys.sh
+++ .api_keys.sh
@@ -1,5 +1,10 @@
 #THIS IS THE PLACE FOR YOUR API KEYS SO THAT YOU WON'T BE HARDCODING IT [...]
 #[...] INTO YOUR SCRIPT, USE THAT FILE CAREFULLY AND DO NOT SHOW THIS FILE PUBLICLY
 
-export GITHUB_TOKEN=""
-export GITHUB_TOKEN2=""
+export GITHUB_TOKEN="" # https://github.com/
+export GITHUB_TOKEN2=""# https://github.com/
+export SHODAN_TOKEN="" # https://shodan.io/
+export CENSYS_TOKEN="" # https://censys.io/
+export VIRUSTOTAL_TOKEN="" # https://www.virustotal.com/
+export RECON_DEV_TOKEN="" # https://recon.dev/
+export CHAOS_TOKEN # https://chaos.projectdiscovery.io/
--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@
 #ROOT INSTALLATIONS
 #create a tools folder in ~/
 mkdir /home/penelope/tools
-cd /home/penelope/tools
+cd /home/penelope/tools || { echo "Failure in cd command"; exit 1; }
 
 
 echo "installing JSParser"
 git clone https://github.com/nahamsec/JSParser.git
-cd JSParser
+cd JSParser || { echo "Failure in cd command"; exit 1; }
 python setup.py install
 chmod +x /home/penelope/tools/JSParser/handler.py
 ln /home/penelope/tools/JSParser/handler.py /home/penelope/PATH
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 echo "done"
 
 #calebstewars pwncat
@@ -24,24 +24,24 @@ pip install base64io
 
 # Wappalyzer
 git clone https://github.com/aliasio/wappalyzer
-cd wappalyzer
+cd wappalyzer || { echo "Failure in cd command"; exit 1; }
 yarn install
 yarn run link
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 # js-beautify
 
 npm -g --force install js-beautify
-cd /home/penelope/tools
+cd /home/penelope/tools || { echo "Failure in cd command"; exit 1; }
 
 
 echo "This will be run from user $UID"
 git clone https://github.com/Cloufish/recon_profile.git
-cd recon_profile
+cd recon_profile || { echo "Failure in cd command"; exit 1; }
 cat zprofile >> /home/penelope/.zprofile
 source /home/penelope/.zprofile
 mkdir /home/penelope/tools
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 echo "done"
 
@@ -57,31 +57,31 @@ chmod +x /home/penelope/tools/github-search/
 ## wpscan
 echo "installing wpscan"
 git clone https://github.com/wpscanteam/wpscan.git
-cd wpscan
+cd wpscan || { echo "Failure in cd command"; exit 1; }
 gem install bundler && bundle install --without test
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 echo "done"
 # relative url extractor
 
 git clone https://github.com/jobertabma/relative-url-extractor.git
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 
 echo "installing dirsearch"
 git clone https://github.com/maurosoria/dirsearch.git
 chmod +x /home/penelope/tools/dirsearch/dirsearch.py
 ln /home/penelope/tools/dirsearch/dirsearch.py /home/penelope/PATH
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 echo "done"
 
 
 echo "installing asnlookup"
 git clone https://github.com/yassineaboukir/asnlookup.git
-cd /home/penelope/tools/asnlookup
+cd /home/penelope/tools/asnlookup || { echo "Failure in cd command"; exit 1; }
 pip install -r requirements.txt
 chmod +x /home/penelope/tools/asnlookup/asnlookup.py
 ln /home/penelope/tools/asnlookup/asnlookup.py /home/penelope/PATH
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 echo "done"
 
 #impacket DO SETUP IT WE NEED SUDO PRIVILEGES TO ACCESS /usr and other root directories. Because of that this script doesn't install this tool
@@ -99,9 +99,9 @@ echo "done"
 
 # Setting up wordlists
 mkdir /home/penelope/tools/SecLists
-cd /home/penelope/tools/SecLists
+cd /home/penelope/tools/SecLists || { echo "Failure in cd command"; exit 1; }
 mkdir Directory-Bruting
-cd Directory-Bruting
+cd Directory-Bruting || { echo "Failure in cd command"; exit 1; }
 wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/directory-list-2.3-small.txt
 wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/directory-list-2.3-medium.txt
 wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/directory-list-2.3-big.txt
@@ -109,7 +109,7 @@ wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/
 cd ..
 mkdir Passwords
 
-cd Passwords 
+cd Passwords  || { echo "Failure in cd command"; exit 1; }
 wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/xato-net-10-million-passwords-1000.txt
 wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/xato-net-10-million-passwords-10000.txt
 wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/xato-net-10-million-passwords-100000.txt
@@ -120,7 +120,16 @@ cd ..
 mkdir Usernames
 wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Usernames/xato-net-10-million-usernames-dup.txt -O ./xato-net-10-million-usernames-medium.txt
 wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Usernames/xato-net-10-million-usernames.txt
-cd /home/penelope/tools
+cd ..
+mkdir Javascript-URLs
+cd Javascript-URLs || { echo "Failure in cd command"; exit 1; }
+wget https://s3.amazonaws.com/assetnote-wordlists/data/automated/httparchive_js_2021_01_28.txt -O js-wordlist.txt
+cd ..
+mkdir Github-Dorks
+cd  Github-Dorks || { echo "Failure in cd command"; exit 1; }
+wget https://raw.githubusercontent.com/obheda12/GitDorker/master/Dorks/alldorksv3
+cd /home/penelope/tools || { echo "Failure in cd command"; exit 1; }
+
 #searchsploit database
 
 #searchsploit -u #TOO MUCH STORAGE IT TAKES, you can disable it if you don't use searchsploit and you're just googling exploits yourself
@@ -132,9 +141,9 @@ cd nuclei/v2/cmd/nuclei/ &&
 go build &&
 chmod +x /home/penelope/tools/nuclei/v2/cmd/nuclei/nuclei
 ln /home/penelope/tools/nuclei/v2/cmd/nuclei/nuclei /home/penelope/PATH
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 git clone https://github.com/projectdiscovery/nuclei-templates.git
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 # FavFreak
  git clone https://github.com/devanshbatham/FavFreak && \
@@ -145,68 +154,68 @@ cd /home/penelope/tools/
  python3 -m pip install mmh3
  chmod +x /home/penelope/tools/FavFreak/favfreak.py
  ln /home/penelope/tools/FavFreak/favfreak.py /home/penelope/PATH
- cd /home/penelope/tools/
+ cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 # massdns
 
  git clone https://github.com/blechschmidt/massdns.git
- cd massdns
+ cd massdns || { echo "Failure in cd command"; exit 1; }
  su -c make penelope
- cd /home/penelope/tools/
+ cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 
 #dnmasscan
 
 git clone https://github.com/rastating/dnmasscan.git
-cd dnmasscan
+cd dnmasscan || { echo "Failure in cd command"; exit 1; }
 chmod +x dnmasscan
 ln /home/penelope/tools/dnmasscan/dnmasscan /home/penelope/PATH
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 
 # masscan
 
 git clone https://github.com/robertdavidgraham/masscan.git
-cd masscan
+cd masscan || { echo "Failure in cd command"; exit 1; }
 su -c make penelope
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 # yay
 
 git clone https://aur.archlinux.org/yay-git.git
-cd yay-git
+cd yay-git || { echo "Failure in cd command"; exit 1; }
 su -c make penelope
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 # polenum
 
-cd /home/penelope/PATH
+cd /home/penelope/PATH || { echo "Failure in cd command"; exit 1; }
 wget https://raw.githubusercontent.com/Wh1t3Fox/polenum/master/polenum.py
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 # nuclei-templates
 
 git clone https://github.com/projectdiscovery/nuclei-templates.git
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 #LinPeas, WinPeas, Linux Exploit suggester-2
  git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git
  mv privilege-escalation-awesome-scripts-suite privesc
- cd privesc
+ cd privesc || { echo "Failure in cd command"; exit 1; }
  wget https://raw.githubusercontent.com/jondonas/linux-exploit-suggester-2/master/linux-exploit-suggester-2.pl
- cd /home/penelope/tools/
+ cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 
 #Bug bounty checklists
 
 mkdir checklists
-cd checklists
+cd checklists || { echo "Failure in cd command"; exit 1; }
 wget https://github.com/KathanP19/HowToHunt/raw/master/CheckList/Web_Checklist_by_Chintan_Gurjar.pdf
 wget https://raw.githubusercontent.com/KathanP19/HowToHunt/master/CheckList/Web-Application-Pentesting-checklist.md
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 
-# bpython and other modules 
+# bpython and other modules
 
 pip install bpython requests beautifulsoup4 lxml
 
@@ -214,58 +223,68 @@ pip install bpython requests beautifulsoup4 lxml
 # SubDomanizer v2
 
 git clone https://github.com/nsonaniya2010/SubDomainizer.git
-cd SubDomainizer
+cd SubDomainizer || { echo "Failure in cd command"; exit 1; }
 pip3 install -r requirements.txt
 ln /home/penelope/tools/SubDomainizer/SubDomainizer.py /home/penelope/PATH
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 # github-subdomains
 
 git clone https://github.com/gwen001/github-subdomains
-cd github-subdomains
+cd github-subdomains || { echo "Failure in cd command"; exit 1; }
 go install
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 #Markdown-PP
 
 git clone https://github.com/jreese/markdown-pp.git
-cd markdown-pp
+cd markdown-pp || { echo "Failure in cd command"; exit 1; }
 pip install MarkdownPP
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 # m4ll0k tools
 git clone https://github.com/m4ll0k/Bug-Bounty-Toolz.git
-cd /home/penelope/Bug-Bounty-Toolz
+cd /home/penelope/Bug-Bounty-Toolz || { echo "Failure in cd command"; exit 1; }
+for script in ./*.sh; do
+
+  ln $PWD/${script} /home/penelope/PATH
+
+done;
+ln /home/penelope/tools/Bug-Bounty-Toolz/getsrc.py  /home/penelope/PATH
 ln /home/penelope/tools/Bug-Bounty-Toolz/getrelationship.py /home/penelope/PATH
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 # gdorklinks.sh ~ jhaddix
 
 #My-Tools
-	#bug bounty framework 
+	#bug bounty framework
 	git clone https://github.com/Cloufish/bug_bounty_framework.git
-	cd bug_bounty_framework
-	ln web_recon.sh /home/penelope/PATH
-	ln markdown_converter.sh /home/penelope/PATH
-	cd /home/penelope/tools/
+	cd bug_bounty_framework || { echo "Failure in cd command"; exit 1; }
+
+  for script in ./*.sh; do
+
+    ln $PWD/${script} /home/penelope/PATH
+
+  done;
+	cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 	# HTB_automation
 	git clone https://github.com/Cloufish/HTB_automation.git
-	cd HTB_automation
+	cd HTB_automation || { echo "Failure in cd command"; exit 1; }
 	ln htb_recon.sh /home/penelope/PATH
 	ln htb_converter_to_markdown.sh /home/penelope/PATH
-	cd /home/penelope/tools/
+	cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 
 
-cd /home/penelope/PATH
+cd /home/penelope/PATH || { echo "Failure in cd command"; exit 1; }
 wget https://gist.githubusercontent.com/jhaddix/1fb7ab2409ab579178d2a79959909b33/raw/e9fea4c0f6982546d90d241bc3e19627a7083e5e/Gdorklinks.sh
 mv Gdorklinks.sh gdorklinks
 chmod +x gdorklinks
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 # enum4linux
 wget https://raw.githubusercontent.com/CiscoCXSecurity/enum4linux/master/enum4linux.pl -O /home/penelope/PATH/enum4linux
 touch /etc/samba/smb.conf
-cd /home/penelope/tools/
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 ## GRANTING 755 PERMISSIONS ON ALL FILES IN PATH
 chown -R penelope /home/penelope/tools
@@ -274,4 +293,4 @@ chown -R penelope /home/penelope/PATH #Takes too much space for Docker container
 chmod -R 755 /home/penelope/PATH
 echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in ~/tools"
 
-## Editing 
+## Editing
